47d5626e79db2fcd2b45a3d1f0d74115a2d685c5
Nour Elfawal <noor882005@gmail.com>
2019-12-13 05:08:51 +0200

    radio work on lower than 8 android

--- app/src/main/java/com/example/radiowebview/MainActivity.java
+++ app/src/main/java/com/example/radiowebview/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.radiowebview;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
@@ -24,7 +27,9 @@ import android.widget.ProgressBar;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//public class MainActivity extends AppCompatActivity implements View.OnClickListener,MediaPlayer.OnPreparedListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
     private WebView webView;
     private WebView webViewRoi;
 
@@ -37,16 +42,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private MediaPlayer player;
 
+    String stream="http://188.165.53.101:8056/stream";
+
+    MediaPlayer mediaPlayer;
+
+    boolean prepared=false;
+
+    boolean started=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeUIElements();
 
-        initializeMediaPlayer();
+//        initializeUIElements();
+//
+//        initializeMediaPlayer();
+
+        buttonPlay=findViewById(R.id.buttonPlay);
+        mediaPlayer=new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//        mediaPlayer.setOnPreparedListener((MediaPlayer.OnPreparedListener) this);
+        new PlayerTask().execute(stream);
+
+        buttonPlay.setEnabled(false);
+        buttonPlay.setText("LOADING");
+        buttonPlay.setOnClickListener(this);
+        webView=findViewById(R.id.webView);
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.loadUrl("https://app.filseka.net/");
+        webView.setWebViewClient(new WebViewClient());
 
     }
-
     @Override
     public void onBackPressed() {
         if(webView.canGoBack()){
@@ -80,89 +110,157 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-
-
-
-    private void initializeUIElements() {
-
-        playSeekBar = (ProgressBar) findViewById(R.id.progressBar1);
-        playSeekBar.setMax(100);
-        playSeekBar.setVisibility(View.INVISIBLE);
-
-        buttonPlay = (Button) findViewById(R.id.buttonPlay);
-        buttonPlay.setOnClickListener(this);
-
-        buttonStopPlay = (Button) findViewById(R.id.buttonStopPlay);
-        buttonStopPlay.setEnabled(false);
-        buttonStopPlay.setOnClickListener(this);
-
-    }
-
-
+    @Override
     public void onClick(View v) {
-        if (v == buttonPlay) {
-            startPlaying();
-        } else if (v == buttonStopPlay) {
-            stopPlaying();
+        if(started){
+            started=false;
+            mediaPlayer.pause();
+            buttonPlay.setText("PLAY");
+        }else {
+            started=true;
+            mediaPlayer.start();
+            buttonPlay.setText("PAUSE");
         }
+
     }
 
-    private void startPlaying() {
-        buttonStopPlay.setEnabled(true);
-        buttonPlay.setEnabled(false);
+//    @Override
+//    public void onPrepared(MediaPlayer mp) {
+//        /** Called when MediaPlayer is ready */
+//            mediaPlayer.start();
+//            Log.e("onPrepared","onPrepared");
+//            prepared=true;
+//
+//
+//    }
 
-        playSeekBar.setVisibility(View.VISIBLE);
-
-        player.prepareAsync();
-
-        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-
-            public void onPrepared(MediaPlayer mp) {
-                player.start();
+    private  class PlayerTask extends AsyncTask <String,Void,Boolean>{
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            try {
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.prepare();
+                prepared=true;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
 
-    }
 
-    private void stopPlaying() {
-        if (player.isPlaying()) {
-            player.stop();
-            player.release();
-            initializeMediaPlayer();
+            return prepared;
         }
 
-        buttonPlay.setEnabled(true);
-        buttonStopPlay.setEnabled(false);
-        playSeekBar.setVisibility(View.INVISIBLE);
-    }
-
-    private void initializeMediaPlayer() {
-        player = new MediaPlayer();
-        try {
-            player.setDataSource("http://radio.hvips.com:8056/stream");
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            buttonPlay.setEnabled(true);
+            buttonPlay.setText("PLAY");
         }
-
-        player.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
-
-            public void onBufferingUpdate(MediaPlayer mp, int percent) {
-                playSeekBar.setSecondaryProgress(percent);
-                Log.i("Buffering", "" + percent);
-            }
-        });
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        if (player.isPlaying()) {
-            player.stop();
+        if(started){
+            mediaPlayer.pause();
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(started){
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(prepared){
+            mediaPlayer.release();
+        }
+    }
+
+    //    private void initializeUIElements() {
+//
+//        playSeekBar = (ProgressBar) findViewById(R.id.progressBar1);
+//        playSeekBar.setMax(100);
+//        playSeekBar.setVisibility(View.INVISIBLE);
+//
+//        buttonPlay = (Button) findViewById(R.id.buttonPlay);
+//        buttonPlay.setOnClickListener(this);
+//
+//        buttonStopPlay = (Button) findViewById(R.id.buttonStopPlay);
+//        buttonStopPlay.setEnabled(false);
+//        buttonStopPlay.setOnClickListener(this);
+//
+//    }
+//
+//
+//    public void onClick(View v) {
+//        if (v == buttonPlay) {
+//            startPlaying();
+//        } else if (v == buttonStopPlay) {
+//            stopPlaying();
+//        }
+//    }
+//
+//    private void startPlaying() {
+//        buttonStopPlay.setEnabled(true);
+//        buttonPlay.setEnabled(false);
+//
+//        playSeekBar.setVisibility(View.VISIBLE);
+//
+//        player.prepareAsync();
+//
+//        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//
+//            public void onPrepared(MediaPlayer mp) {
+//                player.start();
+//            }
+//        });
+//
+//    }
+//
+//    private void stopPlaying() {
+//        if (player.isPlaying()) {
+//            player.stop();
+//            player.release();
+//            initializeMediaPlayer();
+//        }
+//
+//        buttonPlay.setEnabled(true);
+//        buttonStopPlay.setEnabled(false);
+//        playSeekBar.setVisibility(View.INVISIBLE);
+//    }
+//
+//    private void initializeMediaPlayer() {
+//        player = new MediaPlayer();
+//        try {
+//            player.setDataSource("http://radio.hvips.com:8056/stream");
+//        } catch (IllegalArgumentException e) {
+//            e.printStackTrace();
+//        } catch (IllegalStateException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        player.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+//
+//            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+//                playSeekBar.setSecondaryProgress(percent);
+//                Log.i("Buffering", "" + percent);
+//            }
+//        });
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        if (player.isPlaying()) {
+//            player.stop();
+//        }
+//    }
+//
 }
 
